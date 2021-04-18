69c70edbebc31a9c7775fa21bd7e0c315280ace4
jeeva-srinivasan <67260485+jeeva-srinivasan@users.noreply.github.com>
2021-04-18 22:41:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,24 @@
 from flask import Flask,render_template,request
 import pickle
-from  predict import predict_sentiment
+from  predict import predict
 
 
-recc_df=pickle.load(open("recom_engine_cosine.pickle", "rb"))
+recom_df=pickle.load(open("recc_sys_cosine_corr.pickle", "rb"))
 
 app = Flask(__name__)
 
 @app.route("/",methods =["POST","GET"])
 def home():
     if request.method == "POST":
-        user_id = request.form.get("userName")
-        user_id=user_id.lower().strip()
-        if len(user_id)==0:
+        user_name = request.form.get("userName")
+        user_name=user_name.lower().strip()
+        if len(user_name)==0:
             return render_template('base.html') + 'PLEASE ENTER USER ID'
-        if user_id not in recc_df.index:
+        if user_name not in recom_df.index:
             return render_template('base.html') + 'THE USER ID IS NOT AVILABLE IN DATASET PLEASE USE VALID USER ID'
         else:  
-            result_df=predict(user_id,recc_df)
-            return render_template('home.html',predict=result_df.head(5),user=user_id) 
+            result_df=predict(user_name,recom_df)
+            return render_template('home.html',predict=result_df.head(5),user=user_name) 
             
     else:
         return render_template('base.html')  
